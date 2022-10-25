d532d2c400a4b8696b03c9939fb86234e06f4c2c
tiendatAI <tiendatlework@gmail.com>
2022-10-25 16:15:48 +0700

    move to src folder

--- verilog/db/vga_tb.v
+++
@@ -1,8 +0,0 @@
-
-`timescale 1ns/10ps
-
-`include "vga_controller.v"
-
-module vga_tb();
-    
-endmodule
--- verilog/src/main.v
+++ verilog/src/main.v
@@ -1,4 +1,6 @@
 //This file combines all neccessary modules
+`timescale 1ns / 1ps
+
 `include "uart_rx.v"
 `include "vga_controller.v"
 
@@ -10,31 +12,26 @@
 4. Add controller to stack R, G, B in RAM
 */
 
-module main
+module main #(
+        parameter CLKS_PER_BIT = 5208 // (50MHz / 9600 baudrate) = 5208 Clocks Per Bit.
+    ) 
     (
         //rx interface
-        output       o_Rx_DV,
         input        i_Clock_50,  //50MHz clock of DE0 -> Pin R8
         input        i_Rx_Serial, //input signal wire  -> Pin 
         input        i_Reset      //reset button       -> Pin J15
         //vga interface
-
     );
 
     wire [7:0] o_Rx_Byte;       //connect to vga module
-    reg        r_Rx_Serial = 1;
 
-    // Use a 50 MHz clock
-    // Want to interface to 9600 baud UART
-    // 50_000_000 / 9600 = 5208 Clocks Per Bit.
-    parameter c_CLKS_PER_BIT = 5308;
+    // uart_rx #(.CLKS_PER_BIT(CLKS_PER_BIT)) UART_RX_INST (
+    //     .o_Rx_DV    (),
+    //     .o_Rx_Byte  (o_Rx_Byte), 
+    //     .i_Clock    (i_Clock_50),
+    //     .i_Rx_Serial(i_Rx_Serial) //unavailable
+    //     );
     
-    uart_rx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_RX_INST (
-        .o_Rx_DV(),
-        .o_Rx_Byte(o_Rx_Byte), 
-        .i_Clock(i_Clock_50),       
-        .i_Rx_Serial(r_Rx_Serial) //unavailable
-        );
     
     
 
