6c1f205521260338309d5518b32f00d131f21c45
tiendatAI <tiendatlework@gmail.com>
2022-10-26 15:37:35 +0700

    remove files

--- verilog/src/main.v
+++ verilog/src/main.v
@@ -13,27 +13,8 @@
 */
 
 module main #(
-        parameter CLKS_PER_BIT = 5208 // (50MHz / 9600 baudrate) = 5208 Clocks Per Bit.
-    ) 
-    (
-        //uart interface
-        input        i_Clock_50,    //50MHz clock of DE0 -> Pin R8
-        input        i_Rx_Serial,   //input signal wire  -> GPIO
-        input        i_Reset_UART,  //reset button UART  -> Pin J15
-        //vga interface
-        input        i_Reset_VGA,   //reset button VGA   -> Pin E1
-    );
+        
+    )(
 
-    wire [7:0] rx_Byte;       //connect to vga module
-
-    uart_rx #(.CLKS_PER_BIT(CLKS_PER_BIT)) UART_RX_INST (
-        .o_Rx_DV    (),
-        .o_Rx_Byte  (Rx_Byte), 
-        .i_Clock    (i_Clock_50),
-        .i_Rx_Serial(i_Rx_Serial),
-        .i_Reset_n  (i_Reset_n)
-        );
-    
-    
-
+    ); 
 endmodule
