4b70473091303d2dcabe98392458f558546f488f
tiendatAI <tiendatlework@gmail.com>
2022-11-22 22:03:30 +0700

    update script

--- scripts/convert_img.py
+++ scripts/convert_img.py
@@ -1,102 +1,59 @@
 """
 Convert images to hexadecimal arrays
+Write "python convert_img.py -i ./img_test.jpg -o ./converted_img.hex" in terminal
 """
-
 from __future__ import division, absolute_import, print_function
+
 import cv2
-import sys
-import glob
 import logging
-import binascii
-import serial 
+import argparse
+import numpy as np
+from PIL import Image
+from pathlib import Path
 
 #config for logging 
 logging.basicConfig(format="%(asctime)s-%(levelname)s-%(message)s", level=logging.INFO)
 
-
-def serial_ports() -> list:
-    """ Lists serial port names
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
-
-def open_com(port):
-    try:
-        ser = serial.Serial(
-            port= port,
-            baudrate=9600,
-            timeout=0.1,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            xonxoff=False, 
-            rtscts=False, 
-            write_timeout=None, 
-            dsrdtr=False,
-            inter_byte_timeout=None, 
-            exclusive=None
-            )
-        ser.isOpen() # try to open port, if possible print message and proceed with 'while True:'
-        logging.info("Port is opened!")
-    
-    except IOError as error: # if port is already opened, close it and open it again and print message
-        ser.close() # pyright: reportUnboundVariable=false
-        ser.open()
-        logging.info("Port was already open, was closed and opened again!")
-    
-    return ser
-
-
-def convert_hexadecimal(image, shape=(640, 640)):
+def convert_hexadecimal_img(input_path, output_path, output_shape):
     #resize to specific shape
-    image = bytes(cv2.resize(image, shape))
-    
-    return binascii.hexlify(image)
+    image_arr = np.asarray(Image.open(Path(input_path).resolve())) #absolute path 
+    image_arr = np.resize(image_arr, output_shape)    
 
+    with open(Path(output_path).resolve(), "a") as file:
+        for h in range(image_arr.shape[0]):
+            for w in range(image_arr.shape[1]):
+                for c in range(image_arr.shape[2]):
+                    file.write(hex(image_arr[h][w][c]) + "\n")
 
-def main():
-    video = cv2.VideoCapture(0)
-    list_ports = serial_ports()
-    ser = open_com(list_ports[0]) #first port
-    
-    while True:
-        _, frame = video.read()
+def main(args):
+    if args.using_webcam is True:
+        video = cv2.VideoCapture(0)
         
-        #convert to hexadecimal
-        hexdec_img = convert_hexadecimal(frame)
-        ser.write(hexdec_img)
+        while True:
+            _, frame = video.read()
+            
+            #convert to hexadecimal
+            
+            #display
+            cv2.imshow('Webcam', frame)
+            
+            if cv2.waitKey(1) & 0xFF==ord('q'):
+                break
         
-        #display
-        cv2.imshow('Webcam', frame)
-        
-        if cv2.waitKey(1) & 0xFF==ord('q'):
-            break
-    
-    video.release()
-    cv2.destroyAllWindows()
+        video.release()
+        cv2.destroyAllWindows()
+    else:
+        convert_hexadecimal_img(args.input_img, args.output_img, args.output_shape)        
 
 
 if __name__ == '__main__':
-    main() 
+    parser = argparse.ArgumentParser(description="Convert image/video to hexadecimal")
+    parser.add_argument("--using_webcam", default=False, help="Using realtime webcam or only convert images")
+    parser.add_argument("-i", "--input_img", help="Input image path to convert")
+    parser.add_argument("-o", "--output_img", help="Output image path to convert")
+    parser.add_argument("--output_shape", default=(640, 640, 3), help="Output shape of image")
+
+    args = parser.parse_args()
+    ret = main(args)
+    logging.info("Successfully converted!")
+    exit(ret) 
